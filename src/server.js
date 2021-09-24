d41feab507cdf4fa920eb15db863e0df917e6695
hosuk <kirklayer@gmail.com>
2021-09-24 16:14:53 +0900

    ws done

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http"
+import WebSocket from 'ws'
 import express from "express";
 
 const app = express();
@@ -5,8 +7,21 @@ const app = express();
 app.set('view engine', 'pug');
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/" , (req, res) => res.render("home"));
-app.get("/*", (req,res) => res.redirect("/"));
+app.get("/" , (_, res) => res.render("home"));
+app.get("/*", (_,res) => res.redirect("/"));
 
-console.log("hello");
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server})
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅")
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"))
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'))
+    })
+    socket.send("hello!!!")
+})
+
+server.listen(3000, handleListen);
