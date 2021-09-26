28559d59add17c0ab877dffd75ebf1989042da3d
smarthug <kirklayer@gmail.com>
2021-09-27 08:03:59 +0900

    works

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -26,6 +26,9 @@ function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
     socket.send(makeMessage("new_message", input.value));
+    // const li = document.createElement("li")
+    // li.innerText = `You: ${input.value}`;
+    // messageList.append(li);
     input.value = ""
 }
 
@@ -33,6 +36,7 @@ function handleNickSubmit(event) {
     event.preventDefault();
     const input = nickForm.querySelector("input");
     socket.send(makeMessage("nickname", input.value));
+    input.value = ""
 }
 
 messageForm.addEventListener('submit', handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,29 +1,39 @@
-import http from "http"
-import WebSocket from 'ws'
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
+import { parse } from "path";
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/" , (_, res) => res.render("home"));
-app.get("/*", (_,res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server})
+const wss = new WebSocket.Server({ server });
 
 const sockets = [];
 
 wss.on("connection", (socket) => {
-    sockets.push(socket);
-    console.log("Connected to Browser ✅")
-    socket.on("close", () => console.log("Disconnected from the Browser ❌"))
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString()))
-    })
-})
+  sockets.push(socket);
+  socket["nickname"] = "Anon"
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break
+    }
+  });
+});
 
 server.listen(3000, handleListen);
