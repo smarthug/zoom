7528a9e00822291540a466d608a84313ca49be49
hosuk <kirklayer@gmail.com>
2021-10-12 16:03:39 +0900

    rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,9 +5,15 @@ const muteBtn = document.getElementById("mute")
 const cameraBtn = document.getElementById("camera")
 const camerasSelect = document.getElementById("cameras")
 
+
+const call = document.getElementById("call")
+
+call.hidden = true
+
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
 
 async function getCameras(){
   try{
@@ -40,7 +46,7 @@ async function getMedia() {
   }
 }
 
-getMedia();
+
 
 function handleMuteBtn(){
   myStream.getAudioTracks().forEach((track)=>(track.enabled = !track.enabled));
@@ -67,3 +73,31 @@ function handleCameraClick(){
 
 muteBtn.addEventListener("click", handleMuteBtn);
 cameraBtn.addEventListener("click", handleCameraClick);
+
+
+// Welcome Form (join a room )
+
+const welcome = document.getElementById("welcome")
+const welcomeForm = welcome.querySelector("form");
+
+function startMedia(){
+  welcome.hidden = true;
+  call.hidden = false;
+  getMedia();
+}
+
+function handleWelcomeSubmit(event){
+  event.preventDefault()
+  const input = welcomeForm.querySelector("input")
+  socket.emit("join_room",input.value, startMedia)
+  roomName = input.value;
+  input.value = ""
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit)
+
+// Socket Code
+
+socket.on("welcome", () => {
+  console.log("someone joined")
+})
